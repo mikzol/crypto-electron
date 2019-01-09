c5693d813e214a3a6fce9ff36efa9f42698b8ce3
react-sea1010 <worldguru0922@gmail.com>
2019-01-09 13:07:14 -0700

    initial project creation && npm init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # electron_crypto_app
+
+
+application for displaying and alerting Cryptocurrency values
+based on user settings
+
+built with node.js / HTML5 / Electron / Google APIs / Axios HTTP
+
+-- honing skills in new Electron application framework/libraries
---
+++ main.js
@@ -0,0 +1,27 @@
+// define global vars
+const {app, BrowserWindow} = require('electron'); // load modules from electron
+const path = require('path');
+const url = require('url');
+
+// global ref to window object (avoid JS garbage collection)
+let win;
+
+function createWindow() {
+
+    // create window object
+    win = new BrowserWindow({width: 800, height: 600});
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    // open devtools
+    win.webContents.openDevTools();
+
+    // window close emitter
+    win.on('closed', () => {
+        win = null;
+    });
+}
